1dcea39feddb8bcb2c94663712fd1029badc4c41
Shazib Siddique <siddiqueshazib4@gmail.com>
2024-07-15 04:24:12 -0400

    finish the core now left is the css

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
+const path = require("path");
 const session = require('express-session');
 
 const isSignedIn = require('./middleware/is-signed-in.js');
@@ -12,7 +13,7 @@ const isGuest = require('./middleware/is-guest.js');
 const passToView = require('./middleware/pass-user-to-view.js');
 
 const authController = require('./controllers/auth.js');
- const  blogController = require('./controllers/blogApp.js');   
+ const  blogController = require('./controllers/blogapp.js');   
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
@@ -25,6 +26,14 @@ mongoose.connection.on('connected', () => {
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 app.use(morgan('dev'));
+
+
+// new code below this line
+app.use(express.static(path.join(__dirname, "public")));
+
+// new code above this line
+
+
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -57,3 +66,4 @@ app.use('/users/:userId/blogApp', blogController);
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
 });
+
